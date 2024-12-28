4a6f3aa4027c6849aaa382d539029c8b6eef670e
roseduan <roseduan7@163.com>
2024-12-28 10:12:12 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,8 @@ const config = {
   url: 'https://roseduan.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/rubik-pi-3/en/',
+  // baseUrl: '/rubik-pi-3/en/',
+  baseUrl: '/rubik-pi-3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
